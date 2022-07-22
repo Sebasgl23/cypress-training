ebf35527714d48bcfa31b41409bb72d1012b56bf
Sebastián Guardo <33914923+Sebasgl23@users.noreply.github.com>
2022-07-22 12:31:07 -0500

    Static Code Analysis adding missing code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  "env": {
+    "browser": true,
+    "es2021": true,
   },
-  'extends': [
-    'google',
+  "extends": [
+    "google",
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
   },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
-  },
+  "plugins": [
+    "@typescript-eslint",
+    "cypress"],
+  "rules": {
+    "quotes": ["error", "double"],
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-force": "warn",
+    "no-unused-vars": "warn",
+    "require-jsdoc": "warn",
+    "max-len": ["error", {"code": 120}]},
 };
